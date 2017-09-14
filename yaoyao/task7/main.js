57eafc6a4b21b6632ddaec45584e1836a05ae026
zed31124 <1532680685@qq.com>
2017-09-14 17:00:43 +0800

    更新JS UI组件之排序列表

--- yaoyao/task7/main.js
+++ yaoyao/task7/main.js
@@ -35,13 +35,14 @@ function init() {
     let stu4 = new Student('zed', 50, 77, 82)
     let stu5 = new Student('zed2', 80, 66, 99)
     let table = new Table([stu1, stu2, stu3, stu4, stu5], config)
-    content.appendChild(table.renderTable())
+    content.appendChild(table.element)
 }
 
 function Table(data, config) {
-    this.element = document.createElement('table')
-    this.data = data
-    this.sortUp = function (e) {
+    let table = {}
+    table.element = document.createElement('table')
+    table.data = data
+    table.sortUp = function (e) {
         let key = e.target.getAttribute('key')
         this.data.sort(function (prev, next) {
             return prev[key] - next[key]
@@ -49,14 +50,14 @@ function Table(data, config) {
         this.renderData()
         
     }
-    this.sortDown = function (e) {
+    table.sortDown = function (e) {
         let key = e.target.getAttribute('key')
         this.data.sort(function (prev, next) {
             return -(prev[key] - next[key])
         })
         this.renderData()
     }
-    this.renderData = function () {
+    table.renderData = function () {
         this.element.children[1].innerHTML = ''
         for (var index = 0, length = this.data.length; index < length; index++) {
             let tr = document.createElement('tr')
@@ -69,8 +70,7 @@ function Table(data, config) {
             this.element.children[1].appendChild(tr)
         }
     }
-    
-    this.renderTable = function () {
+    table.init = function () {
         let doc = document
         let thead = doc.createElement('thead')
         let tbody = doc.createElement('tbody')
@@ -89,17 +89,13 @@ function Table(data, config) {
                 let triangleUp = doc.createElement('div')
                 triangleUp.className = 'triangle-up'
                 triangleUp.setAttribute('key', config[index].type)
-                triangleUp.addEventListener('click', function (e) {
-                    this.sortUp(e)
-                }.bind(this), false)
+                triangleUp.addEventListener('click', this.sortUp.bind(this), false)
 
                 //降序
                 let triangleDown = doc.createElement('div')
                 triangleDown.className = 'triangle-down'
                 triangleDown.setAttribute('key', config[index].type)
-                triangleDown.addEventListener('click', function (e) {
-                    this.sortDown(e)
-                }.bind(this), false)
+                triangleDown.addEventListener('click', this.sortDown.bind(this), false)
 
                 div.appendChild(triangleUp)
                 div.appendChild(triangleDown)
@@ -111,8 +107,9 @@ function Table(data, config) {
         this.element.appendChild(thead)
         this.element.appendChild(tbody)
         this.renderData()
-        return this.element
     }
+    table.init()
+    return table
 }
 
 function Student(name, chineseGrade, mathGrade, englishGrade) {
